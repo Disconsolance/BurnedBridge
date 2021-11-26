e73fc72f8150381022885c9c0a0b268d15b138e4
Madoka Kaname <juno@kamikaze.careers>
2021-11-26 01:32:19 +0100

    Move processing to Handling

--- VK/Handling.py
+++ VK/Handling.py
@@ -1,6 +1,16 @@
 import vk_api
 from VK import User
 
+async def Process(vk, event):
+    print(event)
+    if event.from_chat is True:
+        print(f"This message came from a group chat! It's {event.peer_id}, but came from {event.user_id}!")
+        User = await CreateUser(vk, event.user_id)
+    else:
+        print(f"This message came from a user! It's {event.user_id}")
+        User = await CreateUser(vk, event.user_id)
+    print(event.text)
+
 async def CreateUser(vk, ID):
     userinfo = vk.users.get(userids=ID, fields="first_name, last_name, screen_name, photo_50")[0]
     print(userinfo)
--- VK/LongPoll.py
+++ VK/LongPoll.py
@@ -14,14 +14,7 @@ async def listen():
         try:
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW:
-                    print(event)
-                    if event.from_chat is True:
-                        print(f"This message came from a group chat! It's {event.peer_id}, but came from {event.user_id}!")
-                        User = await Handling.CreateUser(vk, event.user_id)
-                    else:
-                        print(f"This message came from a user! It's {event.user_id}")
-                        User = await Handling.CreateUser(vk, event.user_id)
-                    print(event.text)
+                    await Handling.Process(vk, event)
         except:
             print('LongPoll has crashed! Awaiting 5 seconds before resuming!')
             traceback.print_exc()
